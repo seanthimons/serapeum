23e7af5b6b3544e9b243c240e7ca3e0730c0c142
seanthimons <thimons.sean@gmail.com>
2026-03-09 13:30:30 -0400

    test(52-01): add unit tests for icon_angles_down helper
    - Test icon_angles_down returns shiny.tag with angles-down icon class
    - Test icon_angles_down accepts additional arguments (class, style)
    - 4 tests passing, no regressions in test suite (319 pass, 13 pre-existing failures)

---
+++ tests/testthat/test-load-more.R
@@ -0,0 +1,27 @@
+# Test icon_angles_down helper (Phase 52)
+
+# Source the theme file to access the icon wrapper
+source("../../R/theme_catppuccin.R", local = TRUE)
+
+test_that("icon_angles_down returns a shiny.tag with angles-down icon", {
+  icon <- icon_angles_down()
+
+  # Check that it returns a shiny tag
+  expect_s3_class(icon, "shiny.tag")
+
+  # Check that it contains the angles-down icon class
+  # Font Awesome 6 uses fa-angles-down class
+  icon_html <- as.character(icon)
+  expect_match(icon_html, "fa-angles-down", fixed = TRUE)
+})
+
+test_that("icon_angles_down accepts additional arguments", {
+  icon <- icon_angles_down(class = "fa-spin")
+
+  # Check that it returns a shiny tag
+  expect_s3_class(icon, "shiny.tag")
+
+  # Check that it includes the additional class
+  icon_html <- as.character(icon)
+  expect_match(icon_html, "fa-spin", fixed = TRUE)
+})
